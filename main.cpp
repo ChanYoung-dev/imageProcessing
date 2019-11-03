b9f3a790642cffa8b4b6efe96287a973399ff6dd
emrhssla <46098949+emrhssla@users.noreply.github.com>
2019-11-03 23:17:59 +0900

    원모양 모자이크

--- main.cpp
+++ main.cpp
@@ -1,19 +1,11 @@
-/*
-Compile : g++ -o $1 $1.c -I/usr/local/include -L/usr/local/lib -lopencv_core
--loencv_highgui -lopencv_imgproc -lopencv_imgcodecs
+#include <iostream>
 
-opencv ���̺귯���� /usr/local/lib �� ��ġ�Ǿ� �ִ� ��쿡 �����ϴ� �������̴�.
-*/
-
-#include <opencv/cv.h>
-#include <opencv/highgui.h>
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
+#include<opencv\cv.h>
+#include<opencv\highgui.h>
+#include<opencv\cxcore.h>
+#include<stdio.h>
 
 #define unsigned char uchar
-
-int histogram[256], cdfOfHisto[256], histogramEqual[256];
 
 uchar** uc_alloc(int size_x, int size_y)
 {
@@ -21,7 +13,7 @@ uchar** uc_alloc(int size_x, int size_y)
 	uchar** m;
 	int i;
 
-	if ((m = (uchar**)calloc(size_y, sizeof(uchar*))) == NULL)
+	if ((m = (uchar * *)calloc(size_y, sizeof(uchar*))) == NULL)
 	{
 		printf("uc_alloc error 1\7\n");
 		exit(0);
@@ -35,7 +27,6 @@ uchar** uc_alloc(int size_x, int size_y)
 		}
 	return m;
 }
-
 void read_ucmatrix(int size_x, int size_y, uchar** ucmatrix, char* filename)
 {
 	int i;
@@ -55,7 +46,6 @@ void read_ucmatrix(int size_x, int size_y, uchar** ucmatrix, char* filename)
 		}
 	fclose(f);
 }
-
 void write_ucmatrix(int size_x, int size_y, uchar** ucmatrix, char* filename)
 {
 	int i;
@@ -76,177 +66,62 @@ void write_ucmatrix(int size_x, int size_y, uchar** ucmatrix, char* filename)
 	fclose(f);
 }
 
-void get_hist(uchar** img, int X_Size, int Y_Size) {
-	int i, j, tmp;
-	double tmp1;
-	int t, tp, range, hrange;
-	CvSize histoSize, cdfSize;
-	IplImage* imgHisto, * cdfImgHisto;
+void mosaic(uchar** img, uchar** out, int Row, int Col, int Block) {
+	int i, j, x, y, tmp, count;
+	for (i = 0; i < Row; i += Block) {
+		for (j = 0; j < Col; j += Block) {
+			tmp = 0;
+			count = 0;
+			for (y = 0; y < Block; y++) {
+				for (x = 0; x < Block; x++) {
+					tmp += img[i + y][j + x];
+					count++;
+				}
 
-	histoSize.width = 256;
-	histoSize.height = 256;
 
-	cdfSize.width = 256;
-	cdfSize.height = 256;
+				tmp /= count;
+				for (y = 0; y < Block; y++) {
+					for (x = 0; x < Block; x++) {
+						out[i + y][j + x] = tmp;
+					}
 
-	imgHisto = cvCreateImage(histoSize, 8, 1);
-	cdfImgHisto = cvCreateImage(cdfSize, 8, 1);
-
-	for (i = 0; i < histoSize.height; i++) {
-		for (j = 0; j < histoSize.width; j++) {
-			((uchar*)(imgHisto->imageData + imgHisto->widthStep * i))[j] = 0;
+				}
+			}
 		}
 	}
-	for (i = 0; i < cdfSize.height; i++) {
-		for (j = 0; j < cdfSize.width; j++) {
-			((uchar*)(cdfImgHisto->imageData + cdfImgHisto->widthStep * i))[j] = 0;
-		}
-	}
-
-	tp = X_Size * Y_Size;
-
-	for (i = 0; i < 256; i++)
-		histogram[i] = 0;
-
-	for (i = 0; i < Y_Size; i++)
-		for (j = 0; j < X_Size; j++)
-			histogram[img[i][j]]++;
-
-	// Find the maximum histogram value
-	tmp1 = 0;
-	for (i = 0; i < 256; ++i) {
-		tmp1 = tmp1 > histogram[i] ? tmp1 : histogram[i];
-	}
-
-	for (i = 0; i < 256; ++i) {
-		tmp = (int)255 * (histogram[i] / tmp1);
-		cvLine(imgHisto, cvPoint(i, 255), cvPoint(i, 255 - tmp), CV_RGB(255, 255, 255), 1, 8, 0);
-
-	}
-	cvShowImage("Histo Line", imgHisto);
-
-	cdfOfHisto[0] = histogram[0];
-
-	for (i = 0; i < 256; i++) {
-		cdfOfHisto[i] = cdfOfHisto[i - 1] + histogram[i];
-	}
-
-	//Draw CDF of Histogram
-	tmp1 = (double)cdfOfHisto[255];
-	for (i = 0; i < 256; i++) {
-		tmp = (int)255 * (cdfOfHisto[i] / tmp1);
-		cvLine(cdfImgHisto, cvPoint(i, 255), cvPoint(i, 255 - tmp), CV_RGB(255, 255, 255), 1, 8, 0);
-	}
-
-	cvShowImage("Original CDF of Histogram", cdfImgHisto);
-
-	range = cdfOfHisto[255] - cdfOfHisto[0];
-	printf("%d  %d\n", tp, range);
-	histogramEqual[0] = 0;
-
-	for (i = 1; i < 256; ++i) {
-		t = (int)ceil(((cdfOfHisto[i] - cdfOfHisto[0]) * 255.0) / range);
-		histogramEqual[i] = (t < 0) ? 0 : (t > 255) ? 255 : t;
-	}
-
-	cvReleaseImage(&imgHisto);
-	cvReleaseImage(&cdfImgHisto);
-	for (i = 0; i < Y_Size; ++i)
-		for (j = 0; j < X_Size; ++j)
-			img[i][j] = histogramEqual[img[i][j]];
-
 }
 
-void get_hist1(uchar** img, int X_Size, int Y_Size) {
-	int i, j, tmp;
-	double tmp1;
-	int t, tp, range, hrange;
-	CvSize histoSize, cdfSize;
-	IplImage* imgHisto, * cdfImgHisto;
+void mosaicCircle(uchar** img, uchar** Result, int Row, int Col, double diameter, int Block)
+{
+   int i, j, x, y, tmp2, c;
+   double tmp, xSqure, ySqure;
+   uchar** rimg;
 
-	histoSize.width = 256;
-	histoSize.height = 256;
+   rimg = uc_alloc(Row, Col);
+   mosaic(img, rimg, Row, Col, Block);
 
-	cdfSize.width = 256;
-	cdfSize.height = 256;
+   for (i = 0; i < Row; i++)
+      for (j = 0; j < Col; j++)
+      {
+         ySqure = (abs(Row / 2 - i)) * (abs(Row / 2 - i));
+         xSqure = (abs(Col / 2 - j)) * (abs(Col / 2 - j));
 
-	imgHisto = cvCreateImage(histoSize, 8, 1);
-	cdfImgHisto = cvCreateImage(cdfSize, 8, 1);
+         tmp = sqrt(ySqure + xSqure);
 
-	for (i = 0; i < histoSize.height; i++) {
-		for (j = 0; j < histoSize.width; j++) {
-			((uchar*)(imgHisto->imageData + imgHisto->widthStep * i))[j] = 0;
-		}
-	}
-	for (i = 0; i < cdfSize.height; i++) {
-		for (j = 0; j < cdfSize.width; j++) {
-			((uchar*)(cdfImgHisto->imageData + cdfImgHisto->widthStep * i))[j] = 0;
-		}
-	}
-
-	tp = X_Size * Y_Size;
-
-	for (i = 0; i < 256; i++)
-		histogram[i] = 0;
-
-	for (i = 0; i < Y_Size; i++)
-		for (j = 0; j < X_Size; j++)
-			histogram[img[i][j]]++;
-
-	// Find the maximum histogram value
-	tmp1 = 0;
-	for (i = 0; i < 256; ++i) {
-		tmp1 = tmp1 > histogram[i] ? tmp1 : histogram[i];
-	}
-
-	for (i = 0; i < 256; ++i) {
-		tmp = (int)255 * (histogram[i] / tmp1);
-		cvLine(imgHisto, cvPoint(i, 255), cvPoint(i, 255 - tmp), CV_RGB(255, 255, 255), 1, 8, 0);
-
-	}
-	cvShowImage("Histo Equal", imgHisto);
-
-	cdfOfHisto[0] = histogram[0];
-
-	for (i = 0; i < 256; i++) {
-		cdfOfHisto[i] = cdfOfHisto[i - 1] + histogram[i];
-	}
-
-	//Draw CDF of Histogram
-	tmp1 = (double)cdfOfHisto[255];
-	for (i = 0; i < 256; i++) {
-		tmp = (int)255 * (cdfOfHisto[i] / tmp1);
-		cvLine(cdfImgHisto, cvPoint(i, 255), cvPoint(i, 255 - tmp), CV_RGB(255, 255, 255), 1, 8, 0);
-	}
-
-	cvShowImage("CDF of Histogram", cdfImgHisto);
-
-	range = cdfOfHisto[255] - cdfOfHisto[0];
-	printf("%d  %d\n", tp, range);
-	histogramEqual[0] = 0;
-
-	for (i = 1; i < 256; ++i) {
-		t = (int)ceil(((cdfOfHisto[i] - cdfOfHisto[0]) * 255.0) / range);
-		histogramEqual[i] = (t < 0) ? 0 : (t > 255) ? 255 : t;
-	}
-
-	cvReleaseImage(&imgHisto);
-	cvReleaseImage(&cdfImgHisto);
-	for (i = 0; i < Y_Size; ++i)
-		for (j = 0; j < X_Size; ++j)
-			img[i][j] = histogramEqual[img[i][j]];
-
+         if (tmp < diameter) Result[i][j] = rimg[i][j];
+         else Result[i][j] = img[i][j];
+      }
 }
 
 int main(int argc, char* argv[])
 {
 
-	int i, j;
+	int i, j, circle_size, mosaic_value;
 	IplImage* cvImg;
 	CvSize imgSize;
-	uchar** img;
+	uchar** img, ** result_img, ** result2_img, ** circle_img, ** circle2_img;
 
-	if (argc != 4)
+	if (argc != 6)
 	{
 		printf("Exe imgData x_size y_size \n");
 		exit(0);
@@ -254,35 +129,28 @@ int main(int argc, char* argv[])
 
 	imgSize.width = atoi(argv[2]);
 	imgSize.height = atoi(argv[3]);
+	circle_size = atoi(argv[4]);
+	mosaic_value = atoi(argv[5]);
+
 	img = uc_alloc(imgSize.width, imgSize.height);
+	result_img = uc_alloc(imgSize.width, imgSize.height);
 	read_ucmatrix(imgSize.width, imgSize.height, img, argv[1]);
+
 	cvImg = cvCreateImage(imgSize, 8, 1);
+
+	mosaicCircle(img, result_img, imgSize.width, imgSize.height, circle_size , mosaic_value);
 
 	for (i = 0; i < imgSize.height; i++)
 		for (j = 0; j < imgSize.width; j++)
 		{
-
-			((uchar*)(cvImg->imageData + cvImg->widthStep * i))[j] = img[i][j];
-
+			((uchar*)(cvImg->imageData + cvImg->widthStep * i))[j] = result_img[i][j];
 		}
 
 	cvNamedWindow(argv[1], 1);
 	cvShowImage(argv[1], cvImg);
 
-	get_hist(img, imgSize.width, imgSize.height);
-
-	for (i = 0; i < imgSize.height; i++)
-		for (j = 0; j < imgSize.width; j++)
-		{
-
-			((uchar*)(cvImg->imageData + cvImg->widthStep * i))[j] = img[i][j];
-
-		}
-
-	cvShowImage("HistoGram Equalizer...", cvImg);
-	get_hist1(img, imgSize.width, imgSize.height);
-
 	cvWaitKey(0);
+
 	cvDestroyWindow("image");
 	cvReleaseImage(&cvImg);
 
